27f0c521b6214ac93805822d8ae24dd782d40e55
Seeranjeevi Ramavel L <114507428+seeranjeeviramavel@users.noreply.github.com>
2025-06-21 16:36:22 +0530

    Update signature-preview.tsx

--- src/components/signature-preview.tsx
+++ src/components/signature-preview.tsx
@@ -1,14 +1,17 @@
-import { templates } from '@/lib/constants';
-import { SignatureData } from '@/lib/types';
+import { templates } from "@/lib/constants";
+import { SignatureData } from "@/lib/types";
 
 interface SignaturePreviewProps {
   signatureData: SignatureData;
 }
 
 export function SignaturePreview({ signatureData }: SignaturePreviewProps) {
-  const SelectedTemplate = templates[signatureData.template as keyof typeof templates];
+  const SelectedTemplate =
+    templates[signatureData.template as keyof typeof templates];
 
   return (
-           <SelectedTemplate data={signatureData} />
+    <div id="signature-preview">
+      <SelectedTemplate data={signatureData} />
+    </div>
   );
 }
